5ddcb5ca66bfbcfaa5548afb47d29a508ff81cf5
kriti8kum <157258583+kriti8kum@users.noreply.github.com>
2024-03-11 17:06:29 +0530

    Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -13,8 +13,7 @@ class B extends A {
     }
 
     public static void main(String[] args) {
-        B objB = new B();
-        String result = objB.meth();
-        System.out.println(result);
+        B obj = new B();
+         obj.meth();
     }
 }
